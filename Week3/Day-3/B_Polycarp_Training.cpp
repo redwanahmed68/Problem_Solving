6507cab0579644fcaa818e20574feeb37c9e58d2
Redwan Ahmed <80761078+redwanahmed68@users.noreply.github.com>
2024-11-12 23:44:50 +0600

    Add files via upload

---
+++ Week3/Day-3/B_Polycarp_Training.cpp
@@ -0,0 +1,33 @@
+// Created on 2024-11-12
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr);                 \
+    cout.tie(nullptr);
+#define nl '\n'
+#define ll long long int
+int main()
+{
+    // int cs = 0;
+    // cout << "Case " << ++cs << ": ";
+    fastio();
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int cnt = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (cnt <= v[i])
+        {
+            cnt++;
+        }
+    }
+    cout << cnt - 1 << nl;
+    return 0;
+}
